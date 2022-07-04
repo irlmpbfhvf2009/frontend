5ac2d4ab61857e4145688d3bb415393454816ef0
irlmpbfhvf2009 <hyprocrite1631@gmail.com>
2022-07-04 15:47:50 +0800

    註冊功能

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -32,19 +32,18 @@
             </a-modal>
 
             <a-modal v-model:visible="signUpVisible" title="註冊" @ok="handleSignUpOk">
-                <a-form ref="formRef" :model="formState" :rules="rules" v-bind="layout" @finish="handleFinish"
-                    @validate="handleValidate" @finishFailed="handleFinishFailed">
+                <a-form>
                     <a-form-item label="信箱">
-                        <a-input placeholder="請輸入信箱" />
+                        <a-input  v-model:value="signUpUser.email" placeholder="請輸入信箱" />
                     </a-form-item>
                     <a-form-item label="暱稱">
-                        <a-input placeholder="請輸入暱稱" />
+                        <a-input  v-model:value="signUpUser.username" placeholder="請輸入暱稱" />
                     </a-form-item>
-                    <a-form-item has-feedback label="密碼" name="password">
-                        <a-input placeholder="請輸入密碼" type="password" />
+                    <a-form-item has-feedback label="密碼">
+                        <a-input  v-model:value="signUpUser.password" placeholder="請輸入密碼" type="password" />
                     </a-form-item>
-                    <a-form-item has-feedback label="確認密碼" name="checkPass">
-                        <a-input placeholder="請再次確認密碼" type="password" autocomplete="off" />
+                    <a-form-item has-feedback label="確認密碼">
+                        <a-input  v-model:value="signUpUser.checkPassword" placeholder="請再次確認密碼" type="password" autocomplete="off" />
                     </a-form-item>
                 </a-form>
             </a-modal>
@@ -58,23 +57,44 @@
 <script>
 import { ref, reactive, computed, onMounted } from 'vue'
 import { login } from "@/api/index.js";
+import { register } from "@/api/index.js";
 export default {
     name: "TheHeader",
     setup() {
         const user = reactive({
             email: "",
-            username: "",
             password: "",
         });
+
+        const signUpUser = reactive({
+            email: "",
+            username: "",
+            password: "",
+            checkPassword: "",
+        });
+
         const signInVisible = ref(false);
+        const signUpVisible = ref(false);
 
         const popSignIn = () => {
             signInVisible.value = true;
         };
 
+        const popSignUp = () => {
+            signUpVisible.value = true;
+        };
+
         const handleSignInOk = () => {
             signIn()
         };
+        const handleSignUpOk = () => {
+            if(signUpUser.password!=signUpUser.checkPassword){
+                alert("兩次密碼不一致")
+                return false;
+            }
+            signUp()
+        };
+
         async function signIn() {
             const res = await login(user);
             console.log(res)
@@ -90,18 +110,17 @@ export default {
             }
         }
 
+        async function signUp() {
+            const res = await register(signUpUser);
+            console.log(res)
+        }
 
 
-        const signUpVisible = ref(false);
 
-        const popSignUp = () => {
-            signUpVisible.value = true;
-        };
-        const handleSignUpOk = () => {
-            signUpVisible.value = false;
-        };
+        
         return {
             user,
+            signUpUser,
             popSignIn,
             popSignUp,
             signInVisible,
